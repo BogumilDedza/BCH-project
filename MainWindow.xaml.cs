2c0accc88fdaf52cef7d361074061afba8633f9a
BogumilDedza <bd23237@wp.pl>
2025-05-08 19:04:28 +0200

    second change

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,10 +16,29 @@ namespace BCH_PROJEKT
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool BCHCodingEnable = false;
+        private bool FastMode = true;
+        private bool noiseGenerationEnabled = false;
         public MainWindow()
         {
             InitializeComponent(); //komentarz
             //komentarz2
         }
+
+        // KOMENDY
+
+        private void SendComandButton_Click(object sender, RoutedEventArgs e)
+        {
+            string command=Box.Text;
+            if (string.IsNullOrEmpty(command))
+            {
+                MessageBox.Show("Proszę wpisz komendę","Pusta komenda",MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            
+        }
+
+
     }
 }
